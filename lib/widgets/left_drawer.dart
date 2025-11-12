677621fa7a8fe40cdfd572189bf59ecaacb04c53
syafitr4 <daffasyafitra12@gmail.com>
2025-11-12 17:04:17 +0700

    tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -52,6 +53,17 @@ class LeftDrawer extends StatelessWidget {
             },
           ),
           ListTile(
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
+            onTap: () {
+                // Route to news list page
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                );
+            },
+        ),
+          ListTile(
             leading: const Icon(Icons.post_add),
             title: const Text('Add News'),
             // Bagian redirection ke NewsFormPage
@@ -61,6 +73,7 @@ class LeftDrawer extends StatelessWidget {
               setelah halaman NewsFormPage sudah dibuat.
 
               */
+
         Navigator.pushReplacement(
         context,
         MaterialPageRoute(
--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
+import 'package:football_news/screens/news_entry_list.dart';
+import 'package:football_news/screens/login.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
 
 
 class ItemCard extends StatelessWidget {
@@ -11,6 +16,7 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+        final request = context.watch<CookieRequest>();
     return Material(
       // Menentukan warna latar belakang dari tema aplikasi.
       color: Theme.of(context).colorScheme.secondary,
@@ -19,7 +25,7 @@ class ItemCard extends StatelessWidget {
 
       child: InkWell(
         // Aksi ketika kartu ditekan.
-        onTap: () {
+        onTap: () async {
           // Menampilkan pesan SnackBar saat kartu ditekan.
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
@@ -29,6 +35,45 @@ class ItemCard extends StatelessWidget {
                 if (item.name == "Add News") {
             Navigator.pushReplacementNamed(context, '/');
           }
+          // Add this after your previous if statements
+else if (item.name == "Logout") {
+    // TODO: Replace the URL with your app's URL and don't forget to add a trailing slash (/)!
+    // To connect Android emulator with Django on localhost, use URL http://10.0.2.2/
+    // If you using chrome,  use URL http://localhost:8000
+    
+    final response = await request.logout(
+        "http://localhost:8000/auth/logout/");
+    String message = response["message"];
+    if (context.mounted) {
+        if (response['status']) {
+            String uname = response["username"];
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text("$message See you again, $uname."),
+            ));
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginPage()),
+            );
+        } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                    content: Text(message),
+                ),
+            );
+        }
+    }
+}
+
+          else if (item.name == "See Football News") {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const NewsEntryListPage()
+        ),
+    );
+}
+
+
         },
         // Container untuk menyimpan Icon dan Text
         child: Container(
